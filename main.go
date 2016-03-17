84177a8380c146a70872b6df4e5c0b374364507f
Scott Merritt <smerritt@pandora.com>
2016-03-17 16:25:39 -0700

    game.go: the beginning

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/srm88/twistr/twistr"
 )
 
@@ -9,8 +8,8 @@ import (
 func main() {
 	ui := twistr.MakeTerminalUI()
 	input := twistr.HackInput{Ui: ui}
-	//state := twistr.NewState(input)
+	state := twistr.NewState(input)
 	cpl := &twistr.CardPlayLog{}
 	input.GetInput(twistr.US, "player card ops|event", cpl)
-	fmt.Println(cpl)
+	twistr.PlayCard(state, cpl)
 }
