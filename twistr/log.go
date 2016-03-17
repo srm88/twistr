0ffb6aa24f19e2c9fb5249ebd14d09590e485e8c
Scott Merritt <smerritt@pandora.com>
2016-03-17 16:55:11 -0700

    CardLog struct for literally all card-related logs

--- twistr/log.go
+++ twistr/log.go
@@ -1,5 +1,21 @@
 package twistr
 
+// OurManInTehran: draw top 5 cards, discard any or all
+
+// AskNotWhatYourCountry: discard N from hand, draw replacements
+
+// Versatile log for any card change. Meaning is entirely context-driven.
+// Memo field is purely for log human-readability.
+// Uses include:
+// Cards removed from deck (hand draw)
+// Added cards to deck (start of midwar)
+// Complete ordering of draw deck (shuffling)
+// Drawing a card from discard (SALTNegotiations)
+type CardLog struct {
+	Memo  string
+	Cards []Card
+}
+
 type CoupLog struct {
 	Country *Country
 	Roll    int
