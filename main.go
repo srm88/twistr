08b920d4da7ee5ae79189840b76a3f967e16eb04
Scott Merritt <smerritt@pandora.com>
2016-03-20 15:13:15 -0700

    GetInput: caller can now constrain accepted answers

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ func main() {
 	ui := twistr.MakeTerminalUI()
 	state := twistr.NewState(ui)
 	cpl := &twistr.CardPlayLog{}
-	twistr.GetInput(ui, twistr.USA, "player card ops|event", cpl)
+	twistr.GetInput(ui, twistr.USA, cpl, "player card ops|event")
 	twistr.PlayCard(state, cpl)
 }
--- twistr/ui.go
+++ twistr/ui.go
@@ -1,7 +1,7 @@
 package twistr
 
-func GetInput(ui UI, player Aff, message string, inp interface{}) {
-	inputStr := ui.Solicit(player, message, nil)
+func GetInput(ui UI, player Aff, inp interface{}, message string, choices ...string) {
+	inputStr := ui.Solicit(player, message, choices)
 	err := Unmarshal(inputStr, inp)
 	for err != nil {
 		inputStr = ui.Solicit(player, err.Error()+"\nTry again?", nil)
