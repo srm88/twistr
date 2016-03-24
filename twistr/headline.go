53238469eae42608fd8894fcc106ec537bd259eb
graciegutman <graciegutman@gmail.com>
2016-03-24 13:27:57 -0700

    fmt

--- twistr/headline.go
+++ twistr/headline.go
@@ -5,14 +5,14 @@ import "fmt"
 func Headline(s *State) {
 	usaHl := SelectCard(s, USA, TheChinaCard)
 	sovHl := SelectCard(s, SOV, TheChinaCard)
-    // XXX: If space race show headline is in play, deal with that yo
+	// XXX: If space race show headline is in play, deal with that yo
 	MessageBoth(s, fmt.Sprintf("USA selects %s, and SOV selects %s", usaHl.Name, sovHl.Name))
 	// Check ops
-    if usaHl.Ops >= sovHl.Ops {
-        PlayEvent(s, USA, usaHl)
-        MessageBoth(s, fmt.Sprintf("USA goes first with %s, Soviets go first with %s", usaHl.Name, sovHl.Name))
-    } else {
-        PlayEvent(s, SOV, sovHl)
-        MessageBoth(s, fmt.Sprintf("Soviets go first with %s, USA goes first with %s", sovHl.Name, usaHl.Name))
-    }
+	if usaHl.Ops >= sovHl.Ops {
+		PlayEvent(s, USA, usaHl)
+		MessageBoth(s, fmt.Sprintf("USA goes first with %s, Soviets go first with %s", usaHl.Name, sovHl.Name))
+	} else {
+		PlayEvent(s, SOV, sovHl)
+		MessageBoth(s, fmt.Sprintf("Soviets go first with %s, USA goes first with %s", sovHl.Name, usaHl.Name))
+	}
 }
