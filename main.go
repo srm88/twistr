b25b763ecc0440bdefd5c49782ca53adeb3d17ac
Scott Merritt <srmerritt88@gmail.com>
2016-03-27 00:13:52 -0700

    main.go makes a log at twistr.aof

--- main.go
+++ main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/srm88/twistr/twistr"
+	"log"
+	"os"
 )
 
 // Temp:
 func main() {
 	ui := twistr.MakeTerminalUI()
-	state := twistr.NewState(ui)
+	aofR, err := os.Open("/tmp/twistr.aof")
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Fatal("Failed to open AOF: ", err.Error())
+		}
+		aofR, err = os.OpenFile("/tmp/twistr.aof", os.O_CREATE|os.O_RDONLY, 0666)
+		if err != nil {
+			log.Fatal("Failed to create AOF: ", err.Error())
+		}
+	}
+	defer aofR.Close()
+	aofW, err2 := os.OpenFile("/tmp/twistr.aof", os.O_WRONLY|os.O_APPEND, 0666)
+	if err2 != nil {
+		log.Fatal("Failed to open AOF: ", err2.Error())
+	}
+	defer aofW.Close()
+	state := twistr.NewState(ui, aofR, aofW)
 	twistr.Start(state)
 	fmt.Println("Nice.")
 }
