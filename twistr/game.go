68219c7b415b29fd537c91f096dd8d614eb18413
graciegutman <graciegutman@users.noreply.github.com>
2016-05-31 14:28:35 -0700

    * OpRealigns
    * OpRealign is a thing now
    
    * japanProtected utility func
    
    * Realign as many times as you have ops, babygurl
    
    * Wire't'up
    
    * Messaging around influence removal

--- twistr/game.go
+++ twistr/game.go
@@ -226,9 +226,21 @@ func ConductOps(s *State, player Aff, card Card, kinds ...OpsKind) {
 	case COUP:
 		OpCoup(s, player, card.Ops)
 	case REALIGN:
-		MessageBoth(s, "realign not implemented")
+		OpRealign(s, player, card.Ops)
 	case INFLUENCE:
 		MessageBoth(s, "influence not implemented")
+	}
+}
+
+func OpRealign(s *State, player Aff, ops int) {
+	for i := 0; i < ops; i++ {
+		target := SelectCountry(s, player, "Realign where?")
+		for !canRealign(s, player, target, false) {
+			target = SelectCountry(s, player, "Oh no you goofed. Realign where?")
+		}
+		rollUSA := SelectRoll(s)
+		rollSOV := SelectRoll(s)
+		realign(s, target, rollUSA, rollSOV)
 	}
 }
 
