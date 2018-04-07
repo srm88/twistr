a7c46d09dd15d047fcbb498cc10de03058f6e7b3
Scott Merritt <srmerritt88@gmail.com>
2018-04-07 00:05:07 -0700

    refactor main.go into twistr/multi.go
    Log to ~/.twistr/twistr.log
    AOF to ~/.twistr/server.log

--- main.go
+++ main.go
@@ -1,192 +1,24 @@
 package main
 
-import "bytes"
-import "flag"
-import "fmt"
 import "github.com/srm88/twistr/twistr"
-import "io"
-import "io/ioutil"
 import "log"
-import "net"
 import "os"
 import "os/signal"
-import "strings"
-
-const (
-	AofDir = "/tmp/twistr"
-)
-
-var (
-	port       int
-	secretPort int
-	serverMode bool
-	state      *twistr.State
-	closers    []io.Closer
-)
-
-func init() {
-	flag.IntVar(&port, "port", 1551, "Server port number")
-	secretPort = port + 1
-	closers = []io.Closer{}
-}
-
-// XXX: should include game name in the file path
-func aofPath() string {
-	return fmt.Sprintf("%s/%s", AofDir, "server.aof")
-}
-
-func loadAof(aofPath string) ([]byte, error) {
-	in, err := os.Open(aofPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return nil, fmt.Errorf("Error reading existing AOF: %s", err.Error())
-		}
-		return nil, nil
-	} else {
-		b := new(bytes.Buffer)
-		if _, err := io.Copy(b, in); err != nil {
-			return nil, fmt.Errorf("Error copying aof bytes to buffer: %s", err.Error())
-		}
-		if err := in.Close(); err != nil {
-			return nil, fmt.Errorf("Error closing aof: %s", err.Error())
-		}
-		return b.Bytes(), nil
-	}
-}
-
-func setupServer(ui twistr.UI, game *twistr.Game, aofPath string) error {
-	// In
-	var history *twistr.History
-	b, err := loadAof(aofPath)
-	if err != nil {
-		return err
-	}
-	if len(b) > 0 {
-		history = twistr.NewHistoryBacklog(ui, string(b))
-	} else {
-		history = twistr.NewHistory(ui)
-	}
-	// Out
-	out, err := os.OpenFile(aofPath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	if err != nil {
-		return err
-	}
-	state = twistr.NewState(history, game, true, twistr.SOV, out)
-	closers = append(closers, out)
-	return nil
-}
-
-// Return writer with which to accept sync'd aof
-func setupClient(ui twistr.UI, game *twistr.Game, replay string) {
-	var history *twistr.History
-	if len(replay) > 0 {
-		history = twistr.NewHistoryBacklog(ui, replay)
-	} else {
-		history = twistr.NewHistory(ui)
-	}
-	state = twistr.NewState(history, game, false, twistr.USA, ioutil.Discard)
-}
-
-func connectHost() string {
-	return "localhost"
-}
-
-func isServer(nc *twistr.NCursesUI) bool {
-	var reply string
-	for {
-		reply = twistr.Solicit(nc, "What mode?", []string{"server", "client"})
-		reply = strings.ToLower(reply)
-		switch reply {
-		case "server":
-			return true
-		case "client":
-			return false
-		}
-	}
-}
-
-func connect() (net.Conn, error) {
-	switch {
-	case serverMode:
-		return twistr.Server(port)
-	default:
-		return twistr.Client(fmt.Sprintf("%s:%d", connectHost(), port))
-	}
-}
-
-func secretConnect() (net.Conn, error) {
-	switch {
-	case serverMode:
-		return twistr.Server(secretPort)
-	default:
-		return twistr.Client(fmt.Sprintf("%s:%d", connectHost(), secretPort))
-	}
-}
-
-func syncAof(aofPath string) {
-	if !serverMode {
-		return
-	}
-	// fails if aof doesn't exist
-	var in io.Reader
-	var err error
-	in, err = os.Open(aofPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			panic(fmt.Sprintf("Failed to open aof to sync ... %s\n", err.Error()))
-		}
-		in = new(bytes.Buffer)
-	}
-	secretConn, err := secretConnect()
-	if err != nil {
-		panic(fmt.Sprintf("Failed to accept client conn to sync ... %s\n", err.Error()))
-	}
-	log.Println("Server syncing aof")
-	if _, err := io.Copy(secretConn, in); err != nil {
-		panic(fmt.Sprintf("Failed while sending sync ... %s\n", err.Error()))
-	}
-	secretConn.Close()
-}
-
-func receiveAof() string {
-	if serverMode {
-		return ""
-	}
-	clientReplay := new(bytes.Buffer)
-	secretConn, err := secretConnect()
-	if err != nil {
-		panic(fmt.Sprintf("Failed to dial server to sync ... %s\n", err.Error()))
-	}
-	log.Println("Client receiving aof sync")
-	if _, err := io.Copy(clientReplay, secretConn); err != nil {
-		panic(fmt.Sprintf("Failed while reading sync ... %s\n", err.Error()))
-	}
-	secretConn.Close()
-	return clientReplay.String()
-}
+import "path/filepath"
 
 // Temp:
 func main() {
-	flag.Parse()
-
-	game := twistr.NewGame()
-	ui := twistr.MakeNCursesUI()
-
-	closers = append(closers, ui)
-	// XXX: revisit 'aof path' and 'is server' for multiple game support
-	serverMode = isServer(ui)
-	path := aofPath()
-
-	var err error
-	if serverMode {
-		syncAof(path)
-		if err = setupServer(ui, game, path); err != nil {
-			panic(fmt.Sprintf("Failed to start game: %s\n", err.Error()))
-		}
-	} else {
-		syncedAof := receiveAof()
-		setupClient(ui, game, syncedAof)
+	logFile, err := os.OpenFile(filepath.Join(twistr.DataDir, "twistr.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
 	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
+	ui := twistr.MakeNCursesUI()
+	match := twistr.NewMatch(ui)
+
+	// XXX: revisit 'aof path' and 'is server' for multiple game support
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt, os.Kill)
@@ -194,9 +26,8 @@ func main() {
 	// This goroutine cleans up
 	go func() {
 		exitcode := <-done
-		for _, c := range closers {
-			c.Close()
-		}
+		ui.Close()
+		match.Close()
 		os.Exit(exitcode)
 	}()
 	// This one forwards the signal to the cleanup routine
@@ -208,14 +39,5 @@ func main() {
 	defer func() {
 		done <- 0
 	}()
-
-	var conn net.Conn
-	if conn, err = connect(); err != nil {
-		panic(fmt.Sprintf("Couldn't connect %s\n", err.Error()))
-	}
-	closers = append(closers, conn)
-	state.LinkIn = twistr.NewCmdIn(conn)
-	state.LinkOut = twistr.NewCmdOut(conn)
-
-	twistr.Start(state)
+	match.Run()
 }
--- twistr/multi.go
+++ twistr/multi.go
@@ -1,7 +1,31 @@
 package twistr
 
+import "bytes"
 import "fmt"
+import "io"
+import "io/ioutil"
+import "log"
 import "net"
+import "os"
+import "os/user"
+import "path/filepath"
+import "strings"
+
+var (
+	DataDir string
+)
+
+func init() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err.Error())
+	}
+	DataDir = filepath.Join(u.HomeDir, ".twistr")
+	if err := os.MkdirAll(DataDir, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+}
 
 // This package will evolve into functions around managing multiple games and
 // connections.
@@ -25,11 +49,221 @@ func Server(port int) (conn net.Conn, err error) {
 	var ln net.Listener
 	ln, err = net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
+		log.Printf("Error listening on %d: %s", port, err.Error())
 		return
 	}
-	return ln.Accept()
+	conn, err = ln.Accept()
+	if err != nil {
+		log.Printf("Error accepting conn: %s", err.Error())
+	}
+	return
 }
 
 func Client(url string) (conn net.Conn, err error) {
-	return net.Dial("tcp", url)
+	conn, err = net.Dial("tcp", url)
+	if err != nil {
+		log.Printf("Error connecting to server: %s", err.Error())
+	}
+	return
+}
+
+func isServer(ui UI) bool {
+	var reply string
+	for {
+		reply = Solicit(ui, "Are you the host or the guest?", []string{"host", "guest"})
+		switch strings.ToLower(reply) {
+		case "host":
+			return true
+		case "guest":
+			return false
+		}
+	}
+}
+
+func NameGame() string {
+	return "server"
+}
+
+func connectHost() string {
+	return "localhost"
+}
+
+func loadAof(aofPath string) ([]byte, error) {
+	in, err := os.Open(aofPath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, fmt.Errorf("Error reading existing AOF: %s", err.Error())
+		}
+		return nil, nil
+	} else {
+		b := new(bytes.Buffer)
+		if _, err := io.Copy(b, in); err != nil {
+			return nil, fmt.Errorf("Error copying aof bytes to buffer: %s", err.Error())
+		}
+		if err := in.Close(); err != nil {
+			return nil, fmt.Errorf("Error closing aof: %s", err.Error())
+		}
+		return b.Bytes(), nil
+	}
+}
+
+type Match struct {
+	UI         UI
+	Port       int
+	SyncPort   int
+	ServerMode bool
+	Name       string
+	Game       *Game
+	State      *State
+	closers    []io.Closer
+	Who        Aff
+	// Connected? Synced?
+	// Peer address / ports?
+}
+
+func NewMatch(ui UI) *Match {
+	return &Match{
+		UI:       ui,
+		Port:     1550,
+		SyncPort: 1551,
+		Name:     NameGame(),
+		Game:     NewGame(),
+		closers:  []io.Closer{}}
+}
+
+func (m *Match) AofPath() string {
+	return fmt.Sprintf("%s.aof", filepath.Join(DataDir, m.Name))
+}
+
+func (m *Match) setupServer() error {
+	// In
+	b, err := loadAof(m.AofPath())
+	if err != nil {
+		return err
+	}
+	var history *History
+	if len(b) > 0 {
+		history = NewHistoryBacklog(m.UI, string(b))
+	} else {
+		history = NewHistory(m.UI)
+	}
+	// Out
+	out, err := os.OpenFile(m.AofPath(), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	m.State = NewState(history, m.Game, true, m.Who, out)
+	m.closers = append(m.closers, out)
+	return nil
+}
+
+func (m *Match) setupClient(aof string) {
+	var history *History
+	if len(aof) > 0 {
+		history = NewHistoryBacklog(m.UI, aof)
+	} else {
+		history = NewHistory(m.UI)
+	}
+	m.State = NewState(history, m.Game, false, m.Who, ioutil.Discard)
+}
+
+func (m *Match) connect() (net.Conn, error) {
+	switch {
+	case m.ServerMode:
+		return Server(m.Port)
+	default:
+		return Client(fmt.Sprintf("%s:%d", connectHost(), m.Port))
+	}
+}
+
+func (m *Match) syncConnect() (net.Conn, error) {
+	switch {
+	case m.ServerMode:
+		return Server(m.SyncPort)
+	default:
+		return Client(fmt.Sprintf("%s:%d", connectHost(), m.SyncPort))
+	}
+}
+
+func (m Match) sendAof() (err error) {
+	// fails if aof doesn't exist
+	var in io.Reader
+	in, err = os.Open(m.AofPath())
+	if err != nil {
+		if !os.IsNotExist(err) {
+			log.Printf("Failed to open aof to sync ... %s\n", err.Error())
+			return
+		}
+		in = new(bytes.Buffer) // what is this
+	}
+	log.Println("Server connecting to sync aof")
+	syncConn, err := m.syncConnect()
+	if err != nil {
+		log.Printf("Failed to accept client conn to sync ... %s\n", err.Error())
+		return
+	}
+	defer syncConn.Close()
+	log.Println("Server syncing aof")
+	if _, err = io.Copy(syncConn, in); err != nil {
+		log.Printf("Failed while sending sync ... %s\n", err.Error())
+		return
+	}
+	log.Printf("Server sent aof")
+	return
+}
+
+func (m *Match) receiveAof() (string, error) {
+	b := new(bytes.Buffer)
+	log.Println("Client connecting to sync aof")
+	syncConn, err := m.syncConnect()
+	if err != nil {
+		log.Printf("Failed to dial server to sync ... %s\n", err.Error())
+		return "", err
+	}
+	defer syncConn.Close()
+	log.Println("Client receiving aof sync")
+	if _, err := io.Copy(b, syncConn); err != nil {
+		log.Printf("Failed while reading sync ... %s\n", err.Error())
+		return "", err
+	}
+	log.Printf("Client received aof")
+	return b.String(), nil
+}
+
+func (m *Match) Run() (err error) {
+	m.ServerMode = isServer(m.UI)
+	if m.ServerMode {
+		m.Who = SOV
+		if err = m.sendAof(); err != nil {
+			return
+		}
+		if err = m.setupServer(); err != nil {
+			return
+		}
+	} else {
+		m.Who = USA
+		aof, err := m.receiveAof()
+		if err != nil {
+			return err
+		}
+		m.setupClient(aof)
+	}
+	var conn net.Conn
+	log.Printf("Connecting")
+	if conn, err = m.connect(); err != nil {
+		return
+	}
+	log.Printf("Connected")
+	m.closers = append(m.closers, conn)
+	m.State.LinkIn = NewCmdIn(conn)
+	m.State.LinkOut = NewCmdOut(conn)
+	log.Printf("Starting")
+	Start(m.State)
+	return nil
+}
+
+func (m *Match) Close() {
+	for _, c := range m.closers {
+		c.Close()
+	}
 }
